b0bf73bc7c3ff68a36c00d872b768003c83a91e8
Sanidhya Mahajan <sanidhya10628@gmail.com>
2021-06-26 19:45:16 +0530

    add .gitignore file

--- index.js
+++ index.js
@@ -73,6 +73,8 @@ app.post('/pincode', async (req, res) => {
     res.render('slots', { slots, newdate })
 })
 
-app.listen(8000, () => {
-    console.log("On Port 8000");
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    // console.log("On Port 8000");
 })
